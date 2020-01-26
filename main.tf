c633c65ca68069f68fd5d69dc79b1645ca566327
Mike Dial <mdial@collabralink.com>
2020-01-25 21:22:18 -0500

    remove 22 sg.. that was for dev only

--- main.tf
+++ main.tf
@@ -14,12 +14,6 @@ resource "aws_security_group" "ecs_host" {
   description = "Allows 8080 traffic"
   vpc_id      = var.vpc_id
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
