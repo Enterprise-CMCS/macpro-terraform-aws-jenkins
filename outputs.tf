691b3935cc40c52a3fb16d2815318d4d804b8ff6
Mike Dial <mdial@collabralink.com>
2020-01-25 21:16:12 -0500

    add ecs host output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "ecs_task_role_id" {
   description = "The IAM role ID attached to the ECS task.  This can be used to attach new policies to the running task."
   value       = aws_iam_role.ecs_task.id
 }
+
+output "ecs_host_role_id" {
+  description = "The IAM role ID attached to the ECS host instance.  This can be used to attach new policies to the ECS host."
+  value       = aws_iam_role.ecs_host.id
+}
