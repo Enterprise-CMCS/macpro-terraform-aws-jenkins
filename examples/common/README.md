219523f6b2191c2a25ea230229e54ba485bdee05
Mike Dial <mdial@collabralink.com>
2020-01-25 22:14:11 -0500

    add common example

---
+++ examples/common/README.md
@@ -0,0 +1,23 @@
+# A common use case for this module.
+
+This is a working terraform example that builds a VPC and a Jenkins with fairly common configuration.
+Notably:
+- A custom instance type is specified for the ECS Host.  (t3.small is uncommonly small, but overriding the default value is common)
+- private subnets are passed to auto_scaling_subnets, indicating the Jenkins host should not be publicly accessible.
+-
+
+## Usage
+
+Currently, there are a few prerequisites to running this code as is:
+1. Update the hosted_zone variable with an AWS Route53 hosted zone for which you have permissions to edit.  Purchase a domain through Route53 if needed.
+2. Update the url variable accordingly, ensuring it falls within your hosted zone.
+3. Update the certificate_arn variable; this should be a certificate in ACM that is valid for the assigned url.
+4. Update the key_name variable with the name of an EC2 key pair that already exists.  Create a key pair if needed.
+5. Run:
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+## Notes
---
+++ examples/common/main.tf
@@ -0,0 +1,58 @@
+##############################################
+# Build this example in US East (N. Virginia)
+##############################################
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+###############################
+# Build a VPC for this example
+###############################
+locals {
+  vpc_cidr     = "10.110.0.0/16"
+  example_name = "common"
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "null_resource" "subnets" {
+  count = length(data.aws_availability_zones.available.names)
+  triggers = {
+    private_subnet = cidrsubnet(local.vpc_cidr, 8, count.index + 1)
+    public_subnet  = cidrsubnet(local.vpc_cidr, 8, count.index + 101)
+  }
+}
+
+module "vpc" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "2.9.0"
+  name                 = local.example_name
+  cidr                 = local.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = null_resource.subnets.*.triggers.private_subnet
+  public_subnets       = null_resource.subnets.*.triggers.public_subnet
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_s3_endpoint   = true
+}
+
+#########################################
+# Build Jenkins Master as an ECS Service
+#########################################
+
+module "jenkins" {
+  source                         = "../.."
+  name                           = local.example_name
+  vpc_id                         = module.vpc.vpc_id
+  instance_type                  = "t3.small"
+  host_key_name                  = "examples"
+  auto_scaling_subnets           = [module.vpc.private_subnets[0]]
+  auto_scaling_availability_zone = data.aws_availability_zones.available.names[0]
+  load_balancer_subnets          = module.vpc.public_subnets
+  url                            = "jenkins-${local.example_name}.examples.cl-demo.com"
+  hosted_zone                    = "cl-demo.com"
+  certificate_arn                = "arn:aws:acm:us-east-1:008087533974:certificate/d3303ea6-6bb5-4ae8-93cc-8bad2ddb1345"
+}
