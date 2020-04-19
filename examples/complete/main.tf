f42b79d3a7d8b159558e1b9efaf816e1c5254de4
Mike Dial <mdial@collabralink.com>
2020-04-19 08:23:14 -0400

    set example source to local module path.  this goes against terraform best practices, but, its what everyone else does and ensures your example uses the local module copy

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -44,7 +44,7 @@ module "vpc" {
 #########################################
 
 module "jenkins" {
-  source                         = "git::ssh://git@gitlab.com/collabralink/delivery/terraform-aws-jenkins.git"
+  source                         = "../.."
   name                           = local.example_name
   vpc_id                         = module.vpc.vpc_id
   host_instance_type             = "t3.small"
